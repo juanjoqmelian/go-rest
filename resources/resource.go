0725ec7bb6d940b1f514a2e23806c3863b348620
juanjoqmelian <jquintanamelian@expedia.com>
2015-11-28 14:19:36 +0000

    Refactoring.

--- resources/resource.go
+++ resources/resource.go
@@ -11,6 +11,12 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
+const (
+	MongoDbHost = "mongodb://192.168.99.100:27017"
+	MongoDbSchema = "go-test"
+	MongoDbUserTable = "user"
+)
+
 type UserWebService interface {
 	NewUser(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
 	GetUser(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
@@ -23,15 +29,9 @@ type DefaultUserWebService struct {
 
 func (DefaultUserWebService) NewUser(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
 
-	requestBody, err := ioutil.ReadAll(request.Body)
-	if err != nil {
-		log.Fatal("Unable to parse request body!", err)
-	}
+	user := assembleUserFromRequest(request)
 
-	user := entities.User{}
-	json.Unmarshal(requestBody, &user)
-
-	session, err := mgo.Dial("mongodb://192.168.99.100:27017")
+	session, err := mgo.Dial(MongoDbHost)
 	if err != nil {
 		log.Fatal("mongo connection not found! ", err)
 	}
@@ -39,7 +39,7 @@ func (DefaultUserWebService) NewUser(writer http.ResponseWriter, request *http.R
 
 	session.SetMode(mgo.Monotonic, true)
 
-	collection := session.DB("go-test").C("user")
+	collection := session.DB(MongoDbSchema).C(MongoDbUserTable)
 	err = collection.Insert(user)
 	if err != nil {
 		log.Fatal("couldn't insert users into mongo! ", err)
@@ -53,7 +53,7 @@ func (DefaultUserWebService) NewUser(writer http.ResponseWriter, request *http.R
 
 func (DefaultUserWebService) GetUsers(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
 
-	session, err := mgo.Dial("mongodb://192.168.99.100:27017")
+	session, err := mgo.Dial(MongoDbHost)
 	if err != nil {
 		log.Fatal("mongo connection not found! ", err)
 	}
@@ -61,7 +61,7 @@ func (DefaultUserWebService) GetUsers(writer http.ResponseWriter, request *http.
 
 	session.SetMode(mgo.Monotonic, true)
 
-	collection := session.DB("go-test").C("user")
+	collection := session.DB(MongoDbSchema).C(MongoDbUserTable)
 
 	result := []entities.User{}
 	err = collection.Find(bson.M{}).All(&result)
@@ -81,7 +81,7 @@ func (DefaultUserWebService) GetUser(writer http.ResponseWriter, request *http.R
 
 	email := params.ByName("email")
 
-	session, err := mgo.Dial("mongodb://192.168.99.100:27017")
+	session, err := mgo.Dial(MongoDbHost)
 	if err != nil {
 		log.Fatal("mongo connection not found! ", err)
 	}
@@ -89,7 +89,7 @@ func (DefaultUserWebService) GetUser(writer http.ResponseWriter, request *http.R
 
 	session.SetMode(mgo.Monotonic, true)
 
-	collection := session.DB("go-test").C("user")
+	collection := session.DB(MongoDbSchema).C(MongoDbUserTable)
 
 	result := entities.User{}
 	err = collection.Find(bson.M{"email": email}).One(&result)
@@ -103,4 +103,17 @@ func (DefaultUserWebService) GetUser(writer http.ResponseWriter, request *http.R
 
 	writer.Header().Set("Content-Type", "application/json")
 	writer.WriteHeader(200)
+}
+
+
+func assembleUserFromRequest(request *http.Request) entities.User {
+
+	requestBody, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		log.Fatal("Unable to parse request body!", err)
+	}
+
+	user := entities.User{}
+	json.Unmarshal(requestBody, &user)
+	return user
 }
